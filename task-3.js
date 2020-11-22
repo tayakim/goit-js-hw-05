a01c6b3f07cf4d244761d11035d7818a26a95423
Tatiana <taulru@yandex.com>
2020-11-23 00:20:56 +0100

    added some tasks

---
+++ task-3.js
@@ -0,0 +1,34 @@
+class Storage {
+    constructor(items) {
+        this.items = items;
+    }
+    getItems() {
+        return this.items;
+    }
+    addItem(item) {
+        let newArray = this.items.push(item);
+        return newArray;
+
+    }
+    removeItem(item) {
+        let delItem = this.items.splice(this.items.indexOf(item), 1);
+        return delItem;
+
+    }
+}
+
+const storage = new Storage([
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
